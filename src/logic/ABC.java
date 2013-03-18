15527ca52dcd910e925f83b4746b11552dc17076
mboro <michal.leszek.boron@gmail.com>
2013-03-18 18:42:19 +0100

    Created eclipse project stub.

---
+++ src/logic/ABC.java
@@ -0,0 +1,28 @@
+package logic;
+
+public class ABC {
+
+	private int x;
+	private int y;
+	
+	public ABC(){
+		x = 0;
+		y = 0;
+	}
+	
+	int getX(){
+		return x;
+	}
+	
+	int getY(){
+		return y;
+	}
+	
+	void setY(int y) {
+		this.y = y;
+	}
+	
+	void setX(int x) {
+		this.x = x;
+	}
+}
---
+++ src/logic/EntryPoint.java
@@ -0,0 +1,13 @@
+package logic;
+
+public class EntryPoint {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("asdfasf");
+	}
+
+}
